16ad651311abbaab2e4873ce79e4fa966814b2b8
rinkumahawar <67892878+rinkumahawar@users.noreply.github.com>
2021-10-01 16:29:59 +0530

    Update 2021_09_30_022709_create_replies_table.php

--- database/migrations/2021_09_30_022709_create_replies_table.php
+++ database/migrations/2021_09_30_022709_create_replies_table.php
@@ -15,12 +15,13 @@ class CreateRepliesTable extends Migration
     {
         Schema::create('replies', function (Blueprint $table) {
             $table->id();
+            $table->string('type')->nullable()->comment('1==main comment, 2==reply');
             $table->unsignedBigInteger('comment_id');
             $table->integer('reply_comment_id');
             $table->unsignedBigInteger('user_id');
             $table->integer('comment_user_id');
             $table->text('comment');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
             $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
         });
