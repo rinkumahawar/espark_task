15470be4458be15ff6bee86ed379b8b33b2f2ede
rinkumahawar <67892878+rinkumahawar@users.noreply.github.com>
2021-10-01 16:30:14 +0530

    Update 2021_09_30_022652_create_comments_table.php

--- database/migrations/2021_09_30_022652_create_comments_table.php
+++ database/migrations/2021_09_30_022652_create_comments_table.php
@@ -18,7 +18,7 @@ class CreateCommentsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('post_id');
             $table->text('comment');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
